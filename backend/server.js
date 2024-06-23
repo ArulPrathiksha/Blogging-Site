9b67b6293924db939f07d2ce533c0c0f84e30617
ArulPrathiksha <128911160+ArulPrathiksha@users.noreply.github.com>
2024-06-23 23:41:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use('/file', express.static('files'));
 
 app.use('/api/posts', postsRoutes);
